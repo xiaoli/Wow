f298ad25cb028ce1f1b741dd69f80ea7e932636b
Xiaoli <lee@nsnq.org>
2020-02-19 10:38:08 +0800

    强制单开，创建互斥体

--- AntiAFK/App.xaml.cs
+++ AntiAFK/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using VMProtect;
 
 namespace AntiAFK
 {
@@ -13,5 +15,23 @@ namespace AntiAFK
     /// </summary>
     public partial class App : Application
     {
+        private static Mutex _mutex = null;
+
+        [VMProtect.Begin]
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            const string appName = "A005B3C99F";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                Application.Current.Shutdown();
+            }
+
+            base.OnStartup(e);
+        }
     }
 }
